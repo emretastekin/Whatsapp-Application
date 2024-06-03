f822e28d40706cc21ed2e0a3900be6feb19265d9
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2024-06-03 10:55:05 +0300

    Whatsapp-Application
    Developed with Flutter

--- whatsapp_app/lib/info.dart
+++ whatsapp_app/lib/info.dart
@@ -1,6 +1,6 @@
 const info = [
   {
-    'name': 'Emreee Taştekin',
+    'name': 'Emre Taştekin',
     'message': 'Hey, how are you doing?',
     'time': '3:53 pm',
     'profilePic': 'entities/profilePhoto2.png',
